e0bc855061d530753362706bd10bba2a49f17ec0
desumn <ilyess.tahar@posteo.net>
2026-03-11 19:54:01 +0100

    Correct a typo

--- lua/cl_init.lua
+++ lua/cl_init.lua
@@ -3,4 +3,4 @@ print("[Roleplay Tools] Starting client")
 
 include("RPTools/UI/cl_whisper_ui.lua")
 include("RPTools/UI/cl_admin_menu.lua")
-include("RPTools/ui/cl_whipser_editor.lua")
+include("RPTools/ui/cl_whisper_editor.lua")
