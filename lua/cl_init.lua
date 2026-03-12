8d8340f0cdb4dbb4d0d0609b285d849f38da02d3
desumn <ilyess.tahar@posteo.net>
2026-03-12 18:12:38 +0100

    Change file names to respect the actual directory names in includes

--- lua/cl_init.lua
+++ lua/cl_init.lua
@@ -1,10 +1,10 @@
 
 print("[Roleplay Tools] Starting client")
 
-include("RPTools/UI/cl_whisper_ui.lua")
-include("RPTools/UI/cl_admin_menu.lua")
-include("RPTools/ui/cl_whisper_editor.lua")
-include("RPTools/whispers/cl_whispers_cache.lua")
+include("rptools/ui/cl_whisper_ui.lua")
+include("rptools/ui/cl_admin_menu.lua")
+include("rptools/ui/cl_whisper_editor.lua")
+include("rptools/whispers/cl_whispers_cache.lua")
 
 net.Receive("rptools_register_list", function (len, ply)
     RPTools.UI.registerList = net.ReadTable()
