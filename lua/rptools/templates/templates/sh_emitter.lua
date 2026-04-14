79e438103bea9e894494e9d1d554b55766b7cdb4
desumn <ilyess.tahar@posteo.net>
2026-04-14 18:46:52 +0200

    feat(templates): add an emitter template

---
+++ lua/rptools/templates/templates/sh_emitter.lua
@@ -0,0 +1,55 @@
+local param = RPTools.Templating.Parameters
+
+RPTools.Templating.RegisterTemplate({
+  name = "emitter",
+  description = "play a sound",
+  parameters = {
+    distance = param.Distance(),
+    angle = param.Angle(),
+    sight = param.CheckSight(),
+    inFlag = param.ShortString("Input Flag", "Flag required for activation", false),
+    invertInFlag = param.Boolean("Invert flag", "invert the input flag condition"),
+    sound = param.ShortString("Sound", "Path to a sound in sound/", true, "action"),
+    pitch = param.Number("pitch", "pitch of the played sound (0-255)", 0, 255, false, 100, "Sound Settings"),
+    volume = param.Number("volume", "volume of the played sound (0-1)", 0, 1, false, 1, "Sound Settings"),
+    global = param.Boolean("Global", "wheter the sound should be played to all players or not", "action")
+  },
+  transformer = SERVER and function(args, context)
+    local conditions = RPTools.Condition.EmptyConditionSet()
+    local distance_cond = RPTools.Condition.Create("distance", nil, "le", args.distance)
+
+    RPTools.Condition.AddToSet(conditions, distance_cond)
+
+    local angle_cond = RPTools.Condition.Create("view_angle", nil, "le", args.angle)
+    RPTools.Condition.AddToSet(conditions, angle_cond)
+
+    if args.sight then
+      local los_cond = RPTools.Condition.Create("line_of_sight", nil, "eq", true)
+      RPTools.Condition.AddToSet(conditions, los_cond)
+    end
+
+    if args.inFlag ~= nil then
+      local op = (args.invertInFlag and "ne") or "eq"
+      local flag_cond = RPTools.Condition.Create("flag", args.inFlag, op, true)
+      RPTools.Condition.AddToSet(conditions, flag_cond)
+    end
+
+    local soundActionType = "play_sound"
+
+    local soundAction = RPTools.Actions.Server.Create(soundActionType, { sound = args.sound, volume = args.volume, pitch = args.pitch })
+
+    local actions = RPTools.Actions.Server.EmptyActionSet()
+    RPTools.Actions.Server.AddToSet(actions, soundAction)
+
+    local node = RPTools.Node.Create(
+      context.position,
+      conditions,
+      0,
+      actions,
+      RPTools.Node.TRIGGER_POLICY.ONE_SHOT,
+      RPTools.Node.SCOPE.SINGLE_PLAYER,
+      0
+    )
+    return { node }
+  end or nil,
+})
