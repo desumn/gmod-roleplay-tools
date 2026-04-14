4c273f88222e76a1609bce9b963780bf8ef9b925
desumn <ilyess.tahar@posteo.net>
2026-04-14 12:10:43 +0200

    Remove obsolete commands

--- lua/rptools/commands/sh_commands.lua
+++ lua/rptools/commands/sh_commands.lua
@@ -145,139 +145,65 @@ function RPTools.Commands.requireAdmin(ply)
 end
 
 local coordinatorStarted = false
-if SERVER then
-  concommand.Add("rptools_list_node", function(ply, _, _, _)
+
+concommand.Add("rptools_start", function (ply)
+  if CLIENT then
+    return
+  end
+
+  if SERVER then
     if not RPTools.Commands.requireAdmin(ply) then
       return
     end
-    local nodes = RPTools.NodeRegister.GetAllNodes()
-    local ids = {}
-    for _, node in ipairs(nodes) do
-      table.insert(ids, RPTools.Node.GetId(node))
-    end
-    PrintTable(ids)
-  end)
 
-  concommand.Add("rptools_inspect", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    if not args[1] then
-      return
-    end
-    local node = RPTools.NodeRegister.GetNodeById(args[1])
-    if not node then
-      return
-    end
-    table.Merge(node, RPTools.Coordinator.GetNodeState(args[1]) or {})
-    PrintTable(node)
-  end)
-
-  concommand.Add("rptools_flag", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    local playerName = args[1]
-    if not playerName then
-      return
-    end
-    local flag = args[2]
-    if not flag then
-      return
-    end
-
-    for _, ply_ in ipairs(RPTools.Utilities.FindPlayerByName(playerName)) do
-      print("Added flag " .. flag .. " to " .. ply_:Nick())
-      RPTools.Blackboard.Write(ply_, flag, true)
-    end
-  end)
-
-  concommand.Add("rptools_unflag", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    local playerName = args[1]
-    if not playerName then
-      return
-    end
-    local flag = args[2]
-    if not flag then
-      return
-    end
-
-    for _, ply_ in ipairs(RPTools.Utilities.FindPlayerByName(playerName)) do
-      print("Removed flag " .. flag .. " from " .. ply_:Nick())
-      RPTools.Blackboard.Write(ply_, flag, nil)
-    end
-  end)
-
-  concommand.Add("rptools_dump_blackboard", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    PrintTable(RPTools.Blackboard.GetAll())
-  end)
-
-  concommand.Add("rptools_reset", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    RPTools.NodeRegister.ClearAll()
-    RPTools.Blackboard.ClearAll()
-    RPTools.Coordinator.ClearAllState()
-  end)
-
-  concommand.Add("rptools_remove", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    if not args[1] then
-      return
-    end
-    local node = RPTools.NodeRegister.GetNodeById(args[1])
-    if not node then
-      return
-    end
-    RPTools.NodeRegister.UnregisterNode(args[1])
-  end)
-
-  concommand.Add("rptools_pause_node", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    if not args[1] then
-      return
-    end
-    local node = RPTools.NodeRegister.GetNodeById(args[1])
-    if not node then
-      return
-    end
-    RPTools.Coordinator.SetNodeRunningState(args[1], RPTools.Coordinator.NODE_STATE.PAUSED)
-  end)
-
-  concommand.Add("rptools_unpause_node", function(ply, _, args, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
-    if not args[1] then
-      return
-    end
-    local node = RPTools.NodeRegister.GetNodeById(args[1])
-    if not node then
-      return
-    end
-    RPTools.Coordinator.SetNodeRunningState(args[1], RPTools.Coordinator.NODE_STATE.RUNNING)
-  end)
-
-  concommand.Add("rptools_toggle", function(ply, _, _, _)
-    if not RPTools.Commands.requireAdmin(ply) then
-      return
-    end
     if coordinatorStarted then
-      RPTools.Coordinator.Stop()
-    else
-      RPTools.Coordinator.Start()
+      RPTools.Commands.PrintToPlayer(ply, "Coordinator is already started!")
+      return
     end
-    coordinatorStarted = not coordinatorStarted
-  end)
-end
+
+    RPTools.Coordinator.Start()
+    RPTools.Commands.PrintToPlayer(ply, "Started coordinator!")
+  end
+end)
+
+
+concommand.Add("rptools_stop", function (ply)
+  if CLIENT then
+    return
+  end
+
+  if SERVER then
+    if not RPTools.Commands.requireAdmin(ply) then
+      return
+    end
+
+    if not coordinatorStarted then
+      RPTools.Commands.PrintToPlayer(ply, "Coordinator is not yet started!")
+      return
+    end
+
+    RPTools.Coordinator.Stop()
+    RPTools.Commands.PrintToPlayer(ply, "Stopped coordinator!")
+  end
+end)
+
+
+concommand.Add("rptools_stop", function (ply)
+  if CLIENT then
+    return
+  end
+
+  if SERVER then
+    if not RPTools.Commands.requireAdmin(ply) then
+      return
+    end
+    
+    if not coordinatorStarted then
+      RPTools.Commands.PrintToPlayer(ply, "Coordinator off.")
+      return
+    else
+      RPTools.Commands.PrintToPlayer(ply, "Coordinator on.")
+      return
+    end
+  end
+end)
