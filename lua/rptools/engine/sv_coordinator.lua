35940032ca2a886bcd5280113e2ddcda971ccdb5
desumn <ilyess.tahar@posteo.net>
2026-04-15 20:10:29 +0200

    refactor(coordinator): simplify the coordinator for further refactors

--- lua/rptools/engine/sv_coordinator.lua
+++ lua/rptools/engine/sv_coordinator.lua
@@ -20,247 +20,7 @@ function RPTools.Coordinator.SetNodeRunningState(nodeId, state)
   hook.Run("RPTools_NodeStateChanged", nodeId, state, oldState)
 end
 
-local nodePlayerState = {}
-
-local function getOrCreatePlayerState(steamId, nodeId)
-  nodePlayerState[steamId] = nodePlayerState[steamId] or {}
-  nodePlayerState[steamId][nodeId] = nodePlayerState[steamId][nodeId]
-    or {
-      wasActivated = false,
-      isActive = false,
-      timer = 0,
-      lastActivationTime = 0,
-    }
-
-  return nodePlayerState[steamId][nodeId]
-end
-
----@param steamid string
----@return table
-function RPTools.Coordinator.GetNodeState(steamid)
-  return nodePlayerState[steamid] or {}
-end
-
-function RPTools.Coordinator.ClearAllState()
-  nodePlayerState = {}
-end
-
-local function createEvaluationContext(ply, node)
-  RPTools.Coordinator.SetNodeRunningState(node.id, RPTools.Coordinator.NODE_STATE.RUNNING)
-
-  return {
-    ply = ply,
-    node = node,
-    nodeId = node.id,
-    state = getOrCreatePlayerState(ply:SteamID64(), node.id),
-    filtered = false,
-  }
-end
-
-local function createEvaluationContexts(plys, nodes)
-  local evaluationContexts = {}
-  for _, ply in ipairs(plys) do
-    for _, node in ipairs(nodes) do
-      table.insert(evaluationContexts, createEvaluationContext(ply, node))
-    end
-  end
-  return evaluationContexts
-end
-
-
-local function filterByPolicy(evaluationContext, time)
-  local policy = evaluationContext.node.triggerPolicy
-  if policy == RPTools.Node.TRIGGER_POLICY.MANUAL then
-    return false
-  elseif policy == RPTools.Node.TRIGGER_POLICY.ONE_SHOT then
-    return not evaluationContext.state.wasActivated
-  elseif policy == RPTools.Node.TRIGGER_POLICY.COOLDOWN then
-    return (time - evaluationContext.state.lastActivationTime) >= evaluationContext.node.cooldownDuration
-  elseif policy == RPTools.Node.TRIGGER_POLICY.CONTINOUS then
-    return true -- filtered later
-  else
-    RPTools.Logs.log(
-      RPTools.Logs.LEVEL.WARNING,
-      logModuleName,
-      "Unknown trigger policy for " .. evaluationContext.nodeId
-    )
-    return false
-  end
-end
-
-local function filter(evaluationContexts, time)
-  for _, evaluationContext in ipairs(evaluationContexts) do
-    if evaluationContext.filtered then
-      continue
-    end
-
-    if not filterByPolicy(evaluationContext, time) then
-      evaluationContext.filtered = true
-      continue
-    end
-  end
-end
-
-local function evaluateConditions(evaluationContexts)
-  for _, evaluationContext in ipairs(evaluationContexts) do
-    if evaluationContext.filtered then
-      continue
-    end
-
-    local contextSuccess, err = pcall(function()
-      evaluationContext.conditionsMet = true
-
-      for _, condition in pairs(evaluationContext.node.conditions) do
-        local source = condition.source
-        local sourceParam = condition.sourceParameter
-        local operator = condition.operator
-        local value = condition.value
-
-        local sourceValue =
-          RPTools.Sources.Server.GetFunction(source)(evaluationContext.ply, evaluationContext.node, sourceParam)
-
-        local conditionResult = RPTools.Operators.GetFunction(operator)(sourceValue, value)
-
-        if not conditionResult then
-          evaluationContext.conditionsMet = false
-          break
-        end
-      end
-    end)
-
-    if not contextSuccess then
-      evaluationContext.filtered = true
-      RPTools.Coordinator.SetNodeRunningState(evaluationContext.nodeId, RPTools.Coordinator.NODE_STATE.ERROR)
-      RPTools.Logs.log(
-        RPTools.Logs.LEVEL.ERROR,
-        logModuleName,
-        "Condition evaluation error on " .. evaluationContext.nodeId .. ": " .. err
-      )
-    end
-  end
-end
-
-local function updateTimers(evaluationContexts, deltaTime)
-  for _, evaluationContext in ipairs(evaluationContexts) do
-    if evaluationContext.filtered then
-      continue
-    end
-
-    evaluationContext.timerReached = false
-
-    if evaluationContext.conditionsMet then
-      evaluationContext.state.timer = evaluationContext.state.timer + deltaTime
-      evaluationContext.timerReached = evaluationContext.state.timer >= evaluationContext.node.requiredTime
-    else
-      evaluationContext.state.timer = 0
-    end
-  end
-end
-
-local function prepare(evaluationContexts)
-  for _, evaluationContext in ipairs(evaluationContexts) do
-    if evaluationContext.filtered then
-      continue
-    end
-    evaluationContext.actionsToExecute = {}
-    evaluationContext.shouldActivate = false
-    evaluationContext.shouldDeactivate = false
-    if evaluationContext.conditionsMet and evaluationContext.timerReached then
-      if
-        evaluationContext.node.triggerPolicy == RPTools.Node.TRIGGER_POLICY.CONTINOUS
-        and evaluationContext.state.isActive
-      then
-        continue
-      end
-
-      for _, action in ipairs(evaluationContext.node.actions) do
-        table.insert(evaluationContext.actionsToExecute, action)
-      end
-      evaluationContext.shouldActivate = true
-      RPTools.Logs.log(
-        RPTools.Logs.LEVEL.INFO,
-        logModuleName,
-        "Node "
-          .. evaluationContext.nodeId
-          .. " will activate for "
-          .. evaluationContext.ply:Nick()
-          .. " ("
-          .. #evaluationContext.actionsToExecute
-          .. " actions)"
-      )
-    else
-      if
-        evaluationContext.node.triggerPolicy == RPTools.Node.TRIGGER_POLICY.CONTINOUS
-        and evaluationContext.state.isActive
-      then
-        evaluationContext.shouldDeactivate = true
-        RPTools.Logs.log(
-          RPTools.Logs.LEVEL.INFO,
-          logModuleName,
-          "Node " .. evaluationContext.nodeId .. " will deactivate for " .. evaluationContext.ply:Nick()
-        )
-      end
-    end
-  end
-end
-
-local function execute(evaluationContexts, time)
-  for _, evaluationContext in ipairs(evaluationContexts) do
-    if evaluationContext.filtered then
-      continue
-    end
-    if evaluationContext.shouldDeactivate then
-      evaluationContext.state.isActive = false
-      hook.Run("RPTools_NodeDeactivated", evaluationContext.ply, table.Copy(evaluationContext.node))
-    end
-    if not evaluationContext.shouldActivate then
-      continue
-    end
-    evaluationContext.state.wasActivated = true
-    evaluationContext.state.isActive = true
-    evaluationContext.state.lastActivationTime = time
-    evaluationContext.state.timer = 0
-    local executionSuccess, error = pcall(function()
-      for _, action in ipairs(evaluationContext.actionsToExecute) do
-        local params = RPTools.Actions.Server.GetParams(action)
-        RPTools.Actions.Server.GetFunction(RPTools.Actions.Server.GetActionType(action))(
-          evaluationContext.ply,
-          evaluationContext.node,
-          params
-        )
-      end
-    end)
-
-    if not executionSuccess then
-      evaluationContext.filtered = true
-      RPTools.Coordinator.SetNodeRunningState(evaluationContext.nodeId, RPTools.Coordinator.NODE_STATE.ERROR)
-      RPTools.Logs.log(
-        RPTools.Logs.LEVEL.ERROR,
-        logModuleName,
-        "Execution error on " .. evaluationContext.nodeId .. ": " .. tostring(error)
-      )
-    else
-      RPTools.Logs.log(
-        RPTools.Logs.LEVEL.INFO,
-        logModuleName,
-        "Node "
-          .. evaluationContext.nodeId
-          .. " activated for "
-          .. evaluationContext.ply:Nick()
-          .. ", "
-          .. #evaluationContext.actionsToExecute
-          .. " actions executed"
-      )
-
-      hook.Run(
-        "RPTools_NodeActivated",
-        evaluationContext.ply,
-        table.Copy(evaluationContext.node),
-        table.Copy(evaluationContext.actionsToExecute)
-      )
-    end
-  end
-end
+local activeNodes = {}
 
 local lastTime = CurTime()
 
@@ -268,33 +28,90 @@ local function isRunning(node)
   return RPTools.Coordinator.GetNodeRunningState(node.id) == RPTools.Coordinator.NODE_STATE.RUNNING
 end
 
+hook.Add("RPTools_NodeCreated", "rptools_coordinator_initial_state", function (id, node)
+  RPTools.Coordinator.SetNodeRunningState(id, RPTools.Coordinator.NODE_STATE.RUNNING)
+end)
+
+hook.Add("RPTools_NodeRemoved", "rptools_coordinator_clean_node_state", function (id)
+  RPTools.Coordinator.SetNodeRunningState(id, nil)
+  activeNodes[id] = nil
+end)
+
+hook.Add("PlayerDisconnected", "rptools_coordinator_clean_player_state", function (ply)
+  for id, state in pairs(activeNodes) do
+    state[ply:SteamID64()] = nil
+  end
+end)
+
 local function mainLoop()
   local time = CurTime()
   local deltaTime = time - lastTime
   lastTime = time
-
+  
   local nodes = RPTools.NodeRegister.GetAllNodes()
+  ---@type RPToolsNode[]
   local runningNodes = {}
   for _, node in ipairs(nodes) do
     if isRunning(node) then
       table.insert(runningNodes, node)
     end
   end
-
+  
   local plys = {}
   for _, ply in player.Iterator() do
     if not ply:IsAdmin() or not ply:GetNW2Bool("rptools_vanish", false) then
       table.insert(plys, ply)
     end
   end
+  
+  for _, node in ipairs(runningNodes) do
+    activeNodes[node.id] = activeNodes[node.id] or {}
+    for _, ply in ipairs(plys) do
+      activeNodes[node.id][ply:SteamID64()] = activeNodes[node.id][ply:SteamID64()] or false
+      
+      local conditionsMet = true
+      local conditionSuccess, conditionsError = pcall(function() 
+        for _, condition in ipairs(node.conditions) do
+          local source = condition.source
+          local param = condition.sourceParameter
+          local operator = condition.operator
+          local value = condition.value
+          local sourceValue = RPTools.Sources.Server.GetFunction(source)(ply, node, param)
+          
+          if not RPTools.Operators.GetFunction(operator)(sourceValue, value) then
+            conditionsMet = false
+            break
+          end
+        end
+      end)
 
-  local evaluationContexts = createEvaluationContexts(plys, nodes)
+      if not conditionSuccess then
+        RPTools.Coordinator.SetNodeRunningState(node.id, RPTools.Coordinator.NODE_STATE.ERROR)
+        RPTools.Logs.log(RPTools.Logs.LEVEL.ERROR, logModuleName, node.id .. " condition evaluation failed: " .. conditionsError)
+        continue
+      end
+      
+      if not conditionsMet then 
+        activeNodes[node.id][ply:SteamID64()] = false
+        continue
+      end
+      
+      if activeNodes[node.id][ply:SteamID64()] then continue   end
+      activeNodes[node.id][ply:SteamID64()] = true
+      local actionSuccess, actionsError = pcall(function ()
+        for _, action in ipairs(node.actions) do
+          local actionFunc = RPTools.Actions.Server.GetFunction(action.actionType)
+          actionFunc(ply, node, action.params)
+        end
+      end)
 
-  filter(evaluationContexts, time)
-  evaluateConditions(evaluationContexts)
-  updateTimers(evaluationContexts, deltaTime)
-  prepare(evaluationContexts)
-  execute(evaluationContexts, time)
+      if not actionSuccess then
+        RPTools.Coordinator.SetNodeRunningState(node.id, RPTools.Coordinator.NODE_STATE.ERROR)
+        RPTools.Logs.log(RPTools.Logs.LEVEL.ERROR, logModuleName, node.id .. " action evaluation failed: " .. actionsError)
+        continue
+      end
+    end
+  end
 end
 
 function RPTools.Coordinator.Start()
