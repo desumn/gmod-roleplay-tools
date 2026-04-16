d617e88b94d9fdb7fb08344d39a1bc61d46b639b
desumn <ilyess.tahar@posteo.net>
2026-04-16 19:37:21 +0200

    feat(dispatcher): dispatch player activations

--- lua/rptools/engine/sv_dispatcher.lua
+++ lua/rptools/engine/sv_dispatcher.lua
@@ -13,3 +13,17 @@ hook.Add("RPTools_NodeActivated", "rptools_dispatch_activated", function(nodeId,
     RPTools.Logs.log(RPTools.Logs.LEVEL.ERROR, logModuleName, node.id .. " action evaluation failed: " .. actionsError)
   end
 end)
+
+hook.Add("RPTools_PlayersActivation", "rptools_dispatch_activated", function(nodeId, node, players)
+  local actionsSuccess, actionsError = pcall(function()
+    for _, action in ipairs(node.actions) do
+      local actionFunc = RPTools.Actions.Server.GetFunction(action.actionType)
+      actionFunc(players, node, action.params)
+    end
+  end)
+
+  if not actionsSuccess then
+    RPTools.Coordinator.SetNodeRunningState(nodeId, RPTools.Coordinator.NODE_STATE.ERROR)
+    RPTools.Logs.log(RPTools.Logs.LEVEL.ERROR, logModuleName, node.id .. " action evaluation failed: " .. actionsError)
+  end
+end)
