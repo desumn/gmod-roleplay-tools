93a217830c8242af9fefb95a8aeeed6796065a79
desumn <ilyess.tahar@posteo.net>
2026-04-17 12:42:55 +0200

    chore: clean old files

--- lua/rptools/sources/sh_flag.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-  RPTools.Sources.Server.RegisterSource("flag", function(ply, node, param)
-    return RPTools.Blackboard.Read(ply, RPTools.Blackboard.Key.Flag(param))
-  end, function(param)
-    return isstring(param)
-  end, function(param)
-    return "flag " .. param
-  end)
-end
